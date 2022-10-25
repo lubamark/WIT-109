3bbf8b1a592a5d1fce27c9b3c3397799afa2681d
lubamark <lubamark@mail.ru>
2022-10-25 17:22:15 +0200

    task 2-3

--- script.js
+++ script.js
@@ -14,9 +14,39 @@ function checkResult1() {
   }
 }
 
+function checkResult2() {
+  let value = document.getElementById('t2').value;
+  let resElement = document.getElementById('r2');
+
+  if (value[0] === 'a' || value[0] === 'а') {
+    resElement.innerText = 'Да'
+  } else {
+    resElement.innerText = 'Нет'
+  }
+}
+
+function checkResult3() {
+  let value = document.getElementById('t3').value;
+  let resElement = document.getElementById('r3');
+
+  let valueStr = String(value);
+  if (valueStr.length === 3) {
+    resElement.innerText = +valueStr[0] + valueStr[1] + valueStr[2]
+  } else {
+    resElement.innerText = 'Нужно ввести 3 цифры'
+  }
+
+}
 
 window.onload = function () {
-  let checkResult1Text = checkResult1.toString();
   let codeBox1 = document.getElementById('code1');
-  codeBox1.innerHTML = checkResult1Text;
+  codeBox1.innerHTML = checkResult1.toString();
+
+  let codeBox2 = document.getElementById('code2');
+  codeBox2.innerHTML = checkResult2.toString();
+
+  let codeBox3 = document.getElementById('code3');
+  codeBox3.innerHTML = checkResult2.toString();
+
+
 }
